d5aee3f2d5ac7fe43207f52cd6aed858e0178030
hinesyuki <hinesyu@gmail.com>
2019-07-29 11:33:47 +0800

    7-29

--- DP.py
+++ DP.py
@@ -96,3 +96,43 @@ class Solution:
                 maxx = p[i]-1
                 res = s[(i-p[i]+1)//2:(i+p[i])//2]
         return res
+
+# 最大矩形
+# 给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix: return 0
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left = [0] * n # initialize left as the leftmost boundary possible
+        right = [n] * n # initialize right as the rightmost boundary possible
+        height = [0] * n
+
+        maxarea = 0
+
+        for i in range(m):
+
+            cur_left, cur_right = 0, n
+            # update height
+            for j in range(n):
+                if matrix[i][j] == '1': height[j] += 1
+                else: height[j] = 0
+            # update left
+            for j in range(n):
+                if matrix[i][j] == '1': left[j] = max(left[j], cur_left)
+                else:
+                    left[j] = 0
+                    cur_left = j + 1
+            # update right
+            for j in range(n-1, -1, -1):
+                if matrix[i][j] == '1': right[j] = min(right[j], cur_right)
+                else:
+                    right[j] = n
+                    cur_right = j
+            # update the area
+            for j in range(n):
+                maxarea = max(maxarea, height[j] * (right[j] - left[j]))
+
+        return maxarea
